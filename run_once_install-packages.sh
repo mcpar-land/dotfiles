b00a1dd7b2fcb13f4c536e38854a1b6d9551bfdd
mcpar-land <john@mcpar.land>
2022-06-01 23:13:39 -0400

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,4 +5,9 @@ sudo apt upgrade
 sudo apt install git \
 	neovim \
 	curl \
-	wget
+	wget \
+	tree
+
+PACKDIR="~/.local/share/nvim/site/pack/packer/start"
+mkdir -p "$PACKDIR"
+git -C "$PACKDIR/packer.nvim" || git clone https://github.com/wbthomason/packer.nvim "$PACKDIR/packer.nvim"
